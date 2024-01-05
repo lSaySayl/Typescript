e63a075a49fcb71f4775bacabd0749af4f91d79a
Camilo Ramirez <camilo.ramirez@pragma.com.co>
2024-01-04 22:28:35 -0500

    - update

--- tipos/any.js
+++ tipos/any.js
@@ -8,3 +8,4 @@ avenger = 150.5555;
 console.log(avenger.toFixed(2));
 console.log(exist);
 console.log(power);
+console.log("hola");
--- tipos/any.ts
+++ tipos/any.ts
@@ -10,3 +10,4 @@ console.log( (<number>avenger).toFixed(2));
 
 console.log(exist);
 console.log(power);
+console.log("hola")
