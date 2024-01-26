c02a06866aeed9eeae46dfe28c42eea11004af25
Camilo Ramirez <camilo.ramirez@pragma.com.co>
2024-01-26 15:27:30 -0500

    - exploring namespaces

--- main.js
+++ main.js
@@ -1,71 +1,12 @@
 "use strict";
-(() => {
-})();
-(() => {
-    class Mutant {
-        mutanPower(id) {
-            return this.name + "-" + this.realName + " - " + this.age + " - " + id;
-        }
-    }
-    const prueba = new Mutant();
-    console.log(prueba.mutanPower("20"));
-})();
-(() => {
-    const client = {
-        name: "Bruce",
-        age: 45,
-        address: {
-            city: "Gotham",
-            id: 123,
-            zip: "123456"
-        },
-        getFullAddress(id) {
-            return this.address.city + " " + this.address.zip + " " + id;
-        }
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    console.log(client.getFullAddress("10"));
-})();
-(() => {
-    const conducirBatimovil = (auto) => {
-        auto.encender = true;
-        auto.velocidadMaxima = 100;
-        auto.acelerar();
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-    const batimovil = {
-        encender: false,
-        velocidadMaxima: 0,
-        acelerar() {
-            console.log('...... runnnnn');
-        }
-    };
-    const guason = {
-        reir: true,
-        comer: true,
-        llorar: false
-    };
-    const reir = (guason) => {
-        if (guason.reir) {
-            console.log("Jajajaj");
-        }
-    };
-    reir(guason);
-    const ciudadGotica = (ciudadanos) => {
-        return ciudadanos.length;
-    };
-    console.log(ciudadGotica(['Flash', 'Batman', 'Superman', 'Acuaman']));
-    class Persona {
-        imprimirBio() {
-            console.log(this.nombre + " " + this.edad + " " + this.sexo + " " + this.estadoCivil);
-        }
-    }
-    const persona = new Persona();
-    persona.edad = 20;
-    console.log(persona.edad);
-})();
-(() => {
-    let addNumbers;
-    addNumbers = (a, b) => {
-        return a + b;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText("abcd"));
 //# sourceMappingURL=main.js.map
